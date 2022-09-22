883667d83c0d923135c53df92de948a54fefb9a7
Oluwabori Ige <igeoluwabori@gmail.com>
2022-09-22 15:01:20 +0100

    new

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -6,10 +6,10 @@ import RightPage from './RightPage';
 export default function Webpage() {
   return (
     <div className="container flex font-serif {Montserrat} ">
-      <div className="container-fluid m-4 w-1/3 sm:w-full md:w-full" >
+      <div className="container-fluid m-4 w-1/3" >
          <LeftPage />
       </div>
-      <div className="container-fluid m-4 w-full md: ">
+      <div className="container-fluid m-4 w-full">
         <RightPage />
       </div>         
         
